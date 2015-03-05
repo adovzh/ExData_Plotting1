8232f0e16f845b4e49643d5af0111fa882742add
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2015-03-05 21:19:05 +1100

    Plot2

---
+++ plot2.R
@@ -0,0 +1,28 @@
+## This scripts expects to read data from 'household_power_consumption.txt' file
+## located in current directory.
+
+# function to read a portion of data from the file
+read.hpc <- function(skip, nrows, header) {
+    read.table("household_power_consumption.txt", sep = ";", header = header,
+               stringsAsFactors = FALSE, na.strings = "?",
+               skip = skip, nrows = nrows,
+               colClasses = c(rep("character", 2), rep("numeric", 7)))
+}
+
+# read header
+hpc <- read.hpc(skip = 0, nrows = 5, header = TRUE)
+hpc.header <- colnames(hpc)
+
+# read data related to a 2007-02-01 and 2007-02-02
+# start and length parameters are determined using command line tools
+hpc <- read.hpc(skip = 66637, nrows = 2880, header = FALSE)
+colnames(hpc) <- hpc.header
+
+# parse and add Timestamp column to the data frame
+hpc <- cbind(Timestamp = strptime(paste(hpc$Date, hpc$Time), format = "%d/%m/%Y %H:%M:%S"), hpc)
+
+# create a plot and save it into png file
+ylabel <- "Global Active Power (kilowatts)"
+png("plot2.png", width = 480, height = 480)
+with(hpc, plot(Timestamp, Global_active_power, type = "l", xlab = "", ylab = ylabel))
+dev.off()
